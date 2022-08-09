e0584f7033696a834be56c1386a0fa956a545559
Daniel Brito <danielbrito@pq.uenf.br>
2022-08-09 15:26:29 -0300

    feat: process repots to return subjects demand

--- academ.py
+++ academ.py
@@ -2,3 +2,19 @@ import files
 import extratos_extractor as ee
 import extratos_aggregate as ea
 
+
+def Find_subject_demand():
+    course_subjects = files.Read_course_subjects()
+
+    students_demanded_subjects = [ee.Student_subjects(ee.PDF_to_string(f'Extratos_Academicos/{filename}'), course_subjects)['demanded']
+                         for filename in files.Fetch_reports_filenames()]
+    subject_demand_count = ea.Count_subjects_demand(students_demanded_subjects)
+
+    return subject_demand_count
+
+
+def main():
+    print(Find_subject_demand())
+
+if __name__ == '__main__':
+    main()
