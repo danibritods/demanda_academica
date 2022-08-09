3f039f1f59d9b411ada2f49c4d5cdca7bec4623c
Daniel Brito <danielbrito@pq.uenf.br>
2022-08-09 15:19:03 -0300

    feat: read course subject dict (in json for now)

--- files.py
+++ files.py
@@ -1,3 +1,17 @@
 """
 Library to find, read and write files
 """
+import json
+
+def Read_JSON_to_Dict(json_file):
+    try:
+        with open(json_file,'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return -1  
+
+def Read_course_subjects():
+    course_subjects = Read_JSON_to_Dict('Config/subjects_dict.json')
+    course_subjects_new_format = {k:{'prerequisites':v} for k,v in course_subjects['prerequisites'].items()}
+    return course_subjects_new_format
+
