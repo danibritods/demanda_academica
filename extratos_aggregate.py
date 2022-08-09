afa1aadba01347d95b473bd3ca1b205b3b2a0ea9
Daniel Brito <danielbrito@pq.uenf.br>
2022-08-09 15:16:24 -0300

    feat: Count (agregate) each subject's demand

--- extratos_aggregate.py
+++ extratos_aggregate.py
@@ -1,3 +1,8 @@
 """
 Library to aggregate students data
 """
+from collections import Counter
+from itertools import chain
+
+def Count_subjects_demand(subject_demand_list):
+    return Counter(chain.from_iterable(subject_demand_list)).most_common()
