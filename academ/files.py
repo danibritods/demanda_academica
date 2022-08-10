d2dab5e7a6d4a7a1c83afc62029e5f3f9d43d240
Daniel Brito <danielbrito@pq.uenf.br>
2022-08-10 15:55:26 -0300

    feat: improve readablity

--- academ/files.py
+++ academ/files.py
@@ -1,47 +1,63 @@
 """
 Library to find, read and write files
 """
-import json, csv
+import json
+import csv
 from os import listdir, getcwd
 import logging
 from datetime import datetime
 
+
 def Read_JSON_to_Dict(json_file):
     try:
-        with open(json_file,'r') as f:
+        with open(json_file, 'r') as f:
             return json.load(f)
     except FileNotFoundError:
-        return -1  
+        return -1
+
+
+def Shape_old_course_subject(course_subjects):
+    shaped = {subject: {'name': name, 'prerequisites': prerequisites}
+              for (subject, name), prerequisites 
+              in zip(course_subjects['names'].items(), course_subjects['prerequisites'].values())}
+    return shaped
+
 
 def Read_course_subjects():
     course_subjects = Read_JSON_to_Dict('config/subjects_dict.json')
-    course_subjects_new_format = {k:{'prerequisites':v,'name':course_subjects['names'][k]} for k,v in course_subjects['prerequisites'].items()}
-    return course_subjects_new_format
+    course_subjects_shaped = Shape_old_course_subject(course_subjects)
+    return course_subjects_shaped
+
 
 def Fetch_filenames(dir):
     return listdir(dir)
 
+
 def Get_current_working_directory():
     return getcwd
+
 
 def Fetch_reports_filenames():
     reports_names = Fetch_filenames('extratos_academicos')
     return reports_names
 
-def Save_CSV(table,csv_file_name):
-    with open(csv_file_name,'w') as csvfile:
-        writer = csv.writer(csvfile) 
+
+def Save_CSV(table, csv_file_name):
+    with open(csv_file_name, 'w') as csvfile:
+        writer = csv.writer(csvfile)
         for row in table:
             writer.writerow(row)
-    logging.info("'"+csv_file_name+"'","sucessfuly written!")
+    logging.info("'"+csv_file_name+"'", "sucessfuly written!")
+
 
 def Today():
     return datetime.today().strftime('%Y-%m-%d')
-    
+
+
 def Save_demand_csv(demand_table):
     filename = f'results/demanda_disciplinas_{Today()}.csv'
-    Save_CSV(demand_table,filename)
+    Save_CSV(demand_table, filename)
+
 
 if __name__ == '__main__':
     pass
-
